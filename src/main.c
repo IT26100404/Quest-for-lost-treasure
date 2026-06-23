c83cc48995d8e9c69d5887f8e0c573b1f5ec8d32
IT26100404 <IT26100404@gmail.com>
2026-06-23 14:23:16 +0530

    Added 15*15 map with border walls

--- src/main.c
+++ src/main.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 
+#define SIZE 15
+char map[SIZE][SIZE];
+
+void initializeMap(){
+
+	int i, j;
+
+	for (i = 0; i < SIZE; i++){
+		for (j = 0; j < SIZE; j++){
+			if (i == 0 || i == SIZE - 1 || j == 0 || j == SIZE - 1){
+				map[i][j] = '#';
+			}
+			else {
+				map[i][j] = '.';
+			}
+		}
+	}
+}
+void printMap(){
+	
+	int i, j;
+
+	for (i = 0; i < SIZE; i++){
+		for (j = 0; j < SIZE; j++){
+			printf("%c ", map[i][j]);
+		}
+	printf("\n");
+	}
+}	
 int main (){
 
-	printf("Quest for the Lost Treasure\n");
+	initializeMap();
+	printMap();
 
 	return 0;
 
