1c76ff11806de2e7f617143d98f2a6aab0c51028
IT26100404 <IT26100404@gmail.com>
2026-06-24 18:01:47 +0530

    Added player structure and random player placement

--- src/main.c
+++ src/main.c
@@ -3,7 +3,45 @@
 #include <time.h>
 
 #define SIZE 15
+
+typedef struct {
+
+	char name[50];
+	int row;
+	int col;
+	int score;
+	int health;
+	int keys;
+	char symbol;
+
+}Player;
+
 char map[SIZE][SIZE];
+
+Player player1;
+
+void placePlayer(){
+
+	int x, y;
+
+	while(1){
+		x = rand() % SIZE;
+		y = rand() % SIZE;
+
+		if(map[x][y] == '.'){
+
+			player1.row = x;
+			player1.col = y;
+			player1.score = 0;
+			player1.health = 100;
+			player1.keys = 0;
+			player1.symbol = '1';
+
+			map[x][y] = player1.symbol;
+			break;
+		}
+	}
+}
 
 void initializeMap(){
 
@@ -56,6 +94,7 @@ int main (){
 
 	initializeMap();
 	placeWalls();
+	placePlayer();
 	printMap();
 
 	return 0;
